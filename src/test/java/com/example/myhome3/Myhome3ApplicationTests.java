5ea0fbd6d935f9242559e2d182ec195b8cd28791
hyojin030 <hyojin030@sookmyung.ac.kr>
2020-11-13 19:02:56 +0900

    init

---
+++ src/test/java/com/example/myhome3/Myhome3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhome3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myhome3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
